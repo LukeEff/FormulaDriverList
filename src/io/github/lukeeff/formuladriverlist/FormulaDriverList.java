aa08571f2525cd42e6fbcd3ded4dba9ba30672d4
lukeeff <luke.matthew.fernandez@gmail.com>
2021-09-22 16:46:19 -0700

    Add sorting methods for the FormulaDriverList

--- src/io/github/lukeeff/formuladriverlist/FormulaDriverList.java
+++ src/io/github/lukeeff/formuladriverlist/FormulaDriverList.java
@@ -1,7 +1,28 @@
 package io.github.lukeeff.formuladriverlist;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class FormulaDriverList {
 
     public static void main(String[] args) {
+    }
+
+    /**
+     * Sorts the list of formula drivers by driver name in alphabetical order.
+     *
+     * @param formulaDrivers list of FormulaDriver references.
+     */
+    private static void sortByDriverName(List<FormulaDriver> formulaDrivers) {
+        formulaDrivers.sort(Comparator.comparing(FormulaDriver::driverName));
+    }
+
+    /**
+     * Sorts the list of formula drivers by the number that corresponds to the driver and vehicle.
+     *
+     * @param formulaDrivers list of FormulaDriver references.
+     */
+    private static void sortByFormulaNumber(List<FormulaDriver> formulaDrivers) {
+        formulaDrivers.sort(Comparator.comparing(FormulaDriver::number));
     }
 }
